aa71cbd5e003e0a91138a7723f157c6bd4e78b85
fajaradiwasentosa <fajaradiwasentosa@live.com>
2013-05-05 08:47:25 +0700

    setup production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Enterprise::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -49,7 +49,7 @@ Enterprise::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.js bootstrap.js.coffee)
+  config.assets.precompile += %w( application.css bootstrap_and_overrides.css.less dashboard.css.less global.less variable.less application.js bootstrap.js.coffee dashboard.js.coffee )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
