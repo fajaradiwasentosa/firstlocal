3cfa1a449562cc0aad41d19b80a18db4281c972e
fajaradiwasentosa <fajaradiwasentosa@live.com>
2013-08-09 13:21:30 +0700

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pratamapolykem::Application.config.secret_token = '2e65e5c644e95563fa0ad033745774c9f4a3b3ff15cf13829e6ee199a55e5502fec279b018d8ec1e70d0afefcf7124b1c9be8332c116950df68e8ebfbd594d3b'
