387ab891fc5ff4c6ccf5e017e4f1eb22080c80db
Mada Aryakusumah <lokermada@gmail.com>
2013-08-24 19:12:04 +0700

    add filter

--- app/admin/carriers.rb
+++ app/admin/carriers.rb
@@ -27,10 +27,16 @@ ActiveAdmin.register Carrier do
   form do |f|
     f.inputs do
       f.input :title
-      f.input :expired_date
+      f.input :expired_date, :as => :datepicker
       f.input :basic_carrier, :as => :ckeditor
       f.input :requirement, :as => :ckeditor
     end
     f.buttons
   end
+
+  filter :title 
+  filter :expired_date 
+  filter :basic_carrier 
+  filter :requirement
+
 end
